f0de5535ec46517c0073678690ae3e76d5902861
devinnwebb <131063623+devinnwebb@users.noreply.github.com>
2023-04-17 19:31:54 -0400

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -11,6 +11,34 @@ Creates a restaurant recommendation system that recommends restaurants based on 
 
 import pandas as pd
 import numpy as np
-
+import csv
 #read the csv 
 
+def read_restaurants(filename):
+    """
+    Read restaurant data from a CSV file.
+
+    Args:
+        filename: The filename of the CSV file to read.
+
+    Returns:
+        A list of dictionaries, where each dictionary represents a restaurant and contains the following keys:
+        - name: the name of the restaurant
+        - cuisine: the type of cuisine served
+        - rating: the restaurant's rating out of 5
+        - location: the city where the restaurant is located
+        - price_range: the restaurant's price range (e.g. "$" for cheap, "$$" for moderate, etc.)
+    """
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        restaurants = []
+        for row in reader:
+            restaurant = {
+                'name': row['Name'],
+                'cuisine': row['Cuisine Style'],
+                'rating': row['Rating'],
+                'location': row['City'],
+                'price_range': row['Price Range']
+            }
+            restaurants.append(restaurant)
+        return restaurants
