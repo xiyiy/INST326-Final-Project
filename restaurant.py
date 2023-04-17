dbc6254aae289d85ae6514d343a17fc0410e5216
xiyiy <123658318+xiyiy@users.noreply.github.com>
2023-04-17 19:10:12 -0400

    Initial Creation of restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,16 @@
+"""
+Team Members: Xiyi Yang, Sean August, Devin Webb, Ildreed Mbami
+INST326
+Professor Daniel Pauw
+
+We are utlizing TripAdvisor's restaurant dataset from Kaggle.com.
+(https://www.kaggle.com/datasets/siddharthmandgi/tripadvisor-restaurant-recommendation-data-usa)
+
+Creates a restaurant recommendation system that recommends restaurants based on the user's preferences
+"""
+
+import pandas as pd
+import numpy as np
+
+#read the csv 
+
