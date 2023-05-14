39ea370f820dd0614d9b3686e25576b49a5da626
heyyee <xy004@umd.edu>
2023-05-13 22:32:17 -0400

    Delete added files

--- INST326/Comprehension 8 example.py
+++
@@ -1,9 +0,0 @@
-"""Comprehension check 8
-"""
-
-import re
-
-with open("speech.txt","r") as file:
-    fileData = f.read()
-    found = re.findall(r"for", fileData)
-    print("Number of for", len(found))
--- INST326/testNote.py
+++
@@ -1,7 +0,0 @@
-import json
-
-# some JSON:
-x = '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
